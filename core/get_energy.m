dfed836f4f7b4565915fd046147e0918cfb41fda
Philipp Flotho <PhilippFlotho@gmail.com>
2022-10-17 18:24:38 +0200

    added the energy based metric

---
+++ core/get_energy.m
@@ -0,0 +1,15 @@
+% Author   : Philipp Flotho
+% Copyright 2022 by Philipp Flotho, All rights reserved.
+
+function energy = get_energy(f, ref, sigma)
+    if nargin < 3
+        sigma = [2, 2, 0.0001];
+    elseif isscalar(sigma)
+        sigma = [sigma, sigma, 0.0001];
+    end
+    [fx, fy] = gradient(imgaussfilt3(double(f), sigma));
+    [refx, refy] = gradient(imgaussfilt3(double(ref), sigma));
+
+    energy = abs(fx - refx) + abs(fy - refy);
+    energy = sum(energy(:));
+end
