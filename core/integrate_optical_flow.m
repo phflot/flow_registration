ea2a01f9c3c6e7b04f465fd7949804cc2165e9f7
Philipp Flotho <PhilippFlotho@gmail.com>
2023-12-06 13:06:06 +0100

    function to integrate an optical flow field (subsequent frames)

---
+++ core/integrate_optical_flow.m
@@ -0,0 +1,22 @@
+% Author   : Philipp Flotho
+% Copyright 2023 by Philipp Flotho, All rights reserved.
+
+function [ c_comp] = integrate_optical_flow(w, interpolation_method)
+%COMPENSATE_SEQUENCE_UV compensates a sequence w.r.t. c_ref
+    
+    c_comp = w;
+    c_ref = w(:, :, :, 1);
+
+    if nargin < 4
+        interpolation_method = 'cubic';
+    end
+    
+    for i = 2:size(w, 4)
+        w_tmp = w(:, :, :, i);
+        % w_tmp = imregister_wrapper_w(w(:, :, :, i), ...
+        %     w(:, :, :, i), c_ref, interpolation_method);
+        [c_comp(:, :, :, i), ~] = imregister_wrapper_w(w_tmp, ...
+            c_comp(:, :, :, i-1), c_ref, interpolation_method);
+        c_comp(:, :, :, i) = c_comp(:, :, :, i) + c_comp(:, :, :, i-1);
+    end
+end
